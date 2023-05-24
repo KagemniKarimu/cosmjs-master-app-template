caae6ed1d11c67a9fa73185a5954580478b6c422
kagemnikarimu <82295340+KagemniKarimu@users.noreply.github.com>
2023-05-24 16:35:44 -0500

    add stargate connection and error handling

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { StargateClient } from "@cosmjs/stargate";
 
 const App = () =>{
+    const [client, setClient] = useState();
+
     useEffect(() => {
         connectStargate();
       }, []);
 
 
     const connectStargate = async () => {
-        const client = await StargateClient.connect(
-          "https://g.w.lavanet.xyz:443/gateway/cos5t/rpc-http/a43dd98c9ea68afa4f167969f48770bb"
-        );
-        console.log(await client.getChainId());
+        try {
+            const stargateClient = await StargateClient.connect(
+              'https://g.w.lavanet.xyz:443/gateway/cos5t/rpc-http/a43dd98c9ea68afa4f167969f48770bb'
+            );
+            setClient(await stargateClient);
+            console.log("CosmJS Stargate Connected @",await stargateClient.getChainId());
+          } catch (error) {
+            console.error('Error connecting to Stargate:', error);
+          }
       };
 
     return (
         <h1> 
             <center>The Beginning of a Masterful dApp!</center>
+            
         </h1>
     )
 }
